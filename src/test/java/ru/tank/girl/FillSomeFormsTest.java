28db48df8c28ac57b084a94561cf3ae7391772cb
ljfbvkontakte@gmail.com <Rikaedo2020>
2021-12-17 17:36:21 +0300

    step 5

---
+++ src/test/java/ru/tank/girl/FillSomeFormsTest.java
@@ -0,0 +1,50 @@
+package ru.tank.girl;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FillSomeFormsTest {
+
+        @BeforeAll
+        static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        }
+
+        @Test
+        void fillForm() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Testname");
+        $("#lastName").setValue("Testlastname");
+        $("#userEmail").setValue("testmail@mail.ru");
+        $(".custom-radio:nth-child(1) .custom-control-label").click();
+        $("#userNumber").setValue("3322332233");
+        $("#dateOfBirthInput").click();
+        $("[class*='month-select']").selectOption("March");
+        $("[class*='year-select']").selectOption("2000");
+        $("[class*='day--031']").click();
+        $("#uploadPicture").uploadFile(new File("1.jpg"));
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $(".custom-checkbox:nth-child(1) .custom-control-label").click();
+        $("#currentAddress").setValue("Testaddress");
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#react-select-4-input").setValue("Jaipur").pressEnter();
+        $("#submit").scrollTo().click();
+        $(".modal-content").shouldHave(
+                text("Testname Testlastname"),
+                text("testmail@mail.ru"),
+                text("Male"),
+                text("3322332233"),
+                text("31 March,2000"),
+                text("Biology"),
+                text("Sports"),
+                text("1.jpg"),
+                text("Testaddress"),
+                text("Rajasthan Jaipur"));
+        }
+        }
