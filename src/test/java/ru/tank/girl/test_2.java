bb8d2f76884a4964cbc1ead2f8c9b85af8641e87
ljfbvkontakte@gmail.com <Rikaedo2020>
2021-12-17 17:05:58 +0300

    step 3

--- src/test/java/ru/tank/girl/test_2.java
+++ src/test/java/ru/tank/girl/test_2.java
@@ -15,13 +15,39 @@ import static com.codeborne.selenide.Selenide.open;
         @BeforeAll
         static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        System.setProperty("selenide.browser", "firefox");
         }
 
         @Test
         void successTest() {
         open("https://demoqa.com/automation-practice-form");
+                $("#firstName").setValue("Test");
+                $("#lastName").setValue("Test");
+                $("#userEmail").setValue("test@gmail.com");
+                $(".custom-radio:nth-child(1) .custom-control-label").click();
+                $("#userNumber").setValue("1231231231");
+                $("#dateOfBirthInput").click();
+                $("[class*='month-select']").selectOption("May");
+                $("[class*='year-select']").selectOption("1995");
+                $("[class*='day--010']").click();
+                $("#uploadPicture").uploadFile(new File("1.jpg"));
+                $("#subjectsInput").setValue("Arts").pressEnter();
+                $(".custom-checkbox:nth-child(2) .custom-control-label").click();
+                $("#currentAddress").setValue("Test");
+                $("#react-select-3-input").setValue("NCR").pressEnter();
+                $("#react-select-4-input").setValue("Dehli").pressEnter();
+                $("#submit").scrollTo().click();
 
+                $(".modal-content").shouldHave(
+                        text("Test Test"),
+                        text("test@gmail.com"),
+                        text("Male"),
+                        text("1231231231"),
+                        text("10 May,1995"),
+                        text("Arts"),
+                        text("Reading"),
+                        text("1.jpg"),
+                        text("Test"),
+                        text("NCR Dehli"));
 
         }
         }
