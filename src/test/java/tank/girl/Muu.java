1cbe8fca15a025b4428ab85c6c421628e270aef4
olga-tankgirl <ljfbvkontakte@gmail.com>
2021-12-17 13:37:55 +0300

    111

---
+++ src/test/java/tank/girl/Muu.java
@@ -0,0 +1,14 @@
+package tank.girl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Muu {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue( true);
+
+    }
+
+
+}
