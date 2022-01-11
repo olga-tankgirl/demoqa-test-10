48b49ec0a1f8f8fa703b406a96ecc7ea5b81b73c
ljfbvkontakte@gmail.com <Rikardo2020>
2022-01-12 00:24:22 +0300

    5 in progress

---
+++ src/test/java/tankgirl/objects/FillFormTest.java
@@ -0,0 +1,54 @@
+package tankgirl.objects;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FillFormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1440x900";
+    }
+
+    @Test
+    @DisplayName("Form filling in web-application")
+    void fillForm() {
+        Objects objects = new Objects();
+
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue(objects.firstName);
+        $("#lastName").setValue(objects.lastName);
+        $("#userEmail").setValue(objects.email);
+        $(".custom-radio:nth-child(1) .custom-control-label").click();
+        $("#userNumber").setValue(objects.phone);
+
+        $("#uploadPicture").uploadFile(new File("src/test/resoures/1.jpg"));
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $(".custom-checkbox:nth-child(1) .custom-control-label").click();
+        $("#currentAddress").setValue(objects.address);
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#react-select-4-input").setValue("Jaipur").pressEnter();
+        $("#submit").scrollTo().click();
+        objects.inputDate("31","March","2000");
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(
+                text(objects.firstName + " " + objects.lastName),
+                text(objects.email),
+                text("Male"),
+                text(objects.phone),
+                text("31 March,2000"),
+                text("Biology"),
+                text("Sports"),
+                text("1.jpg"),
+                text(objects.address),
+                text("Rajasthan Jaipur"));
+    }
+}
---
+++ src/test/java/tankgirl/objects/Objects.java
@@ -0,0 +1,21 @@
+package tankgirl.objects;
+
+import com.github.javafaker.Faker;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class Objects {
+    Faker faker = new Faker();
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+    String address = faker.address().fullAddress();
+    String phone = faker.number().digits(10);
+
+    public void inputDate(String d, String m, String y){
+        $("#dateOfBirthInput").scrollTo().click();
+        $("[class*='month-select']").selectOption(m);
+        $("[class*='year-select']").selectOption(y);
+        $("[class*='day--031']").click();
+    }
+    }
